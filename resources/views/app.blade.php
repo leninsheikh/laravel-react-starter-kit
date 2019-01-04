c2018bafcd86e853db9296cb4b894268e6890512
leninsheikh <justtlenin@gmail.com>
2019-01-04 19:50:43 +0600

    change title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Tasksman</title>
+        <title>Laravel || React</title>
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
